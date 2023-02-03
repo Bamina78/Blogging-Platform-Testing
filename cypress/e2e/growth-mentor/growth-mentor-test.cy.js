64fd495ee728ceb0bffc892f473169713c68997c
Bamina78 <banda.amina2@gmail.com>
2023-02-03 14:42:45 +0100

    Add wrong password and reset password tests

--- cypress/e2e/growth-mentor/growth-mentor-test.cy.js
+++ cypress/e2e/growth-mentor/growth-mentor-test.cy.js
@@ -1,39 +1,63 @@
 import userData from "../../fixtures/example.json";
 
 describe("Growth-Mentor Web Test", () => {
-    beforeEach(() => {
-        cy.visit("https://www.growthmentor.com/");
+  beforeEach(() => {
+    cy.visit("https://www.growthmentor.com/");
+  });
 
-    });
+  const signInLink = "https://app.growthmentor.com/mentees/sign_in"
 
-    // it("Testing SignUp Button", () => {
-    //     cy.get("body > div.layout.wrap.slide > header > nav > div > div.secondary-links.d-none.d-lg-flex > a.sign-up").click();
-    //     cy.get('#growthmentor-input-1').type(`${userData.firstName}`);
-    //     cy.get('#growthmentor-input-2').type(`${userData.lastName}`);
-    //     cy.get('#growthmentor-input-3').type(`${userData.email}`);
-    //     cy.get('#growthmentor-input-4').type(`${userData.password}`);
-    //     cy.get('.tw-cursor-pointer').click();
+  it("Testing Sign Up", () => {
+    cy.get(".sign-up").click();
 
-    // });
+    cy.get("#growthmentor-input-1").type(`${userData.firstName}`);
+    cy.get("#growthmentor-input-2").type(`${userData.lastName}`);
+    cy.get("#growthmentor-input-3").type(`${userData.email}`);
+    cy.get("#growthmentor-input-4").type(`${userData.password}`);
 
-    it("Testing SignIn", () => {
-        cy.get('.sign-up').click();
-        cy.get(':nth-child(6) > .tw-underline').click();
-        cy.get('#growthmentor-input-6').type(`${userData.email}`);
-        cy.get('#growthmentor-input-7').type(`${userData.password}`);
-        cy.get('#growthmentor-checkbox-8').click();
-        cy.get('#app > div > div > div > form > button').click();   
+    cy.get(".tw-cursor-pointer").click();
+  });
 
+  it("Testing Wrong Password", () => {
+    cy.get(`[href="${signInLink}"]`).click();
 
-        //Alias - Nickname for selector
-        cy.get('.tw-divide-y > :nth-child(1) > .tw-flex > .tw-text-sm > .tw-text-base').as('signup-text');
-        cy.get(':nth-child(2) > .tw-flex > .tw-text-sm > .tw-text-base').as('signup-subtext');
+    //Test login inputs
+    cy.get("#growthmentor-input-1").type(`${userData.wrongEmail}`);
+    cy.get("#growthmentor-input-2").type(`${userData.wrongPassword}`);
 
-        //Should contain - Find a text 
-        cy.get('@signup-text').should('contain', 'Founder or Co-Founder')
-        cy.get('@signup-subtext').contains('Head of Growth')
+    //Test remember me checkbox
+    cy.get("#growthmentor-checkbox-3").check();
 
+    //Test sign in button
+    cy.get(".tw-cursor-pointer").click();
 
-    });
- 
+    //Test if wrong password error message exist
+    cy.get(".tw-font-semibold").contains("Invalid Email or password.");
+  });
+
+  it("Testing Forgot Password", () => {
+    cy.get(`[href="${signInLink}"]`).click();
+
+    cy.get(".tw-text-neutral-400").click();
+    cy.get("#growthmentor-input-4").type(userData.email);
+
+    cy.get(".tw-shadow-md").click();
+    cy.get(".tw-ml-3 > .tw-font-medium").should("contain", "Success");
+  });
+
+  it("Testing Sign In", () => {
+    cy.get(`[href="${signInLink}"]`).click();
+
+    //Test login inputs
+    cy.get("#growthmentor-input-1").type(`${userData.email}`);
+    cy.get("#growthmentor-input-2").type(`${userData.password}`);
+
+    //Test remember me checkbox
+    cy.get("#growthmentor-checkbox-3").check();
+
+    //Test sign in button
+    cy.get(".tw-cursor-pointer").click();
+  });
+
+  
 });
